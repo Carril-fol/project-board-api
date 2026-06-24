26e4022220af7b9e2ae18aafc7c9a39fe1353f38
Folco Carril <folco.carril@gmail.com>
2026-06-24 15:26:22 -0300

    feat(main): Added router from projects

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from fastapi import FastAPI
 
 from users.controllers.user_controller import router as user_router
 from auth.controllers.auth_controller import router as auth_router
+from projects.controllers.project_controller import router as project_router
 
 app = FastAPI()
 
 app.include_router(user_router)
 app.include_router(auth_router)
+app.include_router(project_router)
 
 if __name__ == "__main__":
     run(app)
