b3cce05b0930cdbbbfdb87559345331991ac321b
Folco Carril <folco.carril@gmail.com>
2026-07-17 23:02:08 -0300

    chore(Auth): Modified endpoint from functions to class with function to centralize

--- src/auth/controllers/auth_controller.py
+++ src/auth/controllers/auth_controller.py
@@ -1,84 +1,87 @@
-from fastapi import APIRouter, HTTPException, Response, Depends
+from fastapi import APIRouter, Depends, HTTPException, Response
+from fastapi_restful.cbv import cbv
 
-from core.security.password_manager import PasswordManager
 from core.security.jwt_manager import JwtManager, jwt_required
-
-from users.dependencies import get_user_repository
-from users.repositories.user_repository import UserRepository
 
 from ..dependencies import get_auth_service
 from ..exceptions.auth_exception import InvalidCredentialsException
-from ..schemas.auth_schema import RegisterInputSchema, RegisterOutputSchema, LoginOutputSchema, LoginInputSchema, LogoutOutputSchema
+from ..schemas.auth_schema import (
+    LoginInputSchema,
+    LoginOutputSchema,
+    LogoutOutputSchema,
+    RegisterInputSchema,
+    RegisterOutputSchema,
+)
 from ..services.auth_services import AuthService
-
 
 router = APIRouter(prefix="/auth/api/v1", tags=["auth"])
 
-password_manager = PasswordManager()
-user_repo: UserRepository = Depends(get_user_repository)
-auth_service = AuthService(user_repo, password_manager)
 
+@cbv(router)
+class AuthController:
+    service: AuthService = Depends(get_auth_service)
+    payload: dict = Depends(jwt_required)
 
-@router.post(
-        "/register", 
-        response_model=RegisterOutputSchema, 
-        status_code=201
+    @router.post(
+        "/register",
+        response_model=RegisterOutputSchema,
+        status_code=201,
     )
-async def register(
-    data: RegisterInputSchema,
-    auth_service: AuthService = Depends(get_auth_service)
-):
-    auth_service.register_user(data)
+    def register(
+        self,
+        data: RegisterInputSchema,
+    ):
+        self.service.register_user(data)
 
-    return RegisterOutputSchema(
-        msg="Register successfully"
+        return RegisterOutputSchema(msg="Register successfully")
+
+    @router.post(
+        "/login",
+        response_model=LoginOutputSchema,
+        status_code=200,
     )
+    def login(
+        self,
+        data: LoginInputSchema,
+        response: Response,
+        jwt_manager: JwtManager = Depends(),
+    ):
+        try:
+            user_id = self.service.authenticate(data)
 
+            access_token = jwt_manager.create_access_token(user_id=user_id)
+            refresh_token = jwt_manager.create_refresh_token(user_id=user_id)
 
-@router.post(
-        "/login", 
-        response_model=LoginOutputSchema, 
-        status_code=200
+            response.set_cookie(key="access_token", value=access_token)
+            response.set_cookie(key="refresh_token", value=refresh_token)
+
+            return {"access_token": access_token, "refresh_token": refresh_token}
+
+        except InvalidCredentialsException:
+            raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    @router.post("/logout", response_model=LogoutOutputSchema, status_code=200)
+    def logout(
+        self,
+        response: Response,
+    ):
+        response.delete_cookie("access_token")
+        response.delete_cookie("refresh_token")
+
+        return LogoutOutputSchema(msg="Logout successfully")
+
+    @router.get(
+        "/refresh",
+        response_model=LoginOutputSchema,
+        status_code=200,
     )
-async def login(
-    data: LoginInputSchema,
-    response: Response,
-    auth_service: AuthService = Depends(get_auth_service),
-    jwt_manager: JwtManager = Depends(),
-):
-    try:
-        user_id = auth_service.authenticate(data)        
+    def refresh(self, response: Response):
+        user_id = self.payload["sub"]
 
-        access_token = jwt_manager.create_access_token(user_id=user_id)
-        refresh_token = jwt_manager.create_refresh_token(user_id=user_id)
+        access_token = JwtManager.create_access_token(user_id=user_id)
+        refresh_token = JwtManager.create_refresh_token(user_id=user_id)
 
         response.set_cookie(key="access_token", value=access_token)
         response.set_cookie(key="refresh_token", value=refresh_token)
 
-        return {
-            "access_token": access_token, 
-            "refresh_token": refresh_token
-        }
-    
-    except InvalidCredentialsException:
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid credentials"
-        )
-
-
-@router.post(
-        "/logout", 
-        response_model=LogoutOutputSchema, 
-        status_code=200
-    )
-async def logout(
-    response: Response,
-    payload: dict = Depends(jwt_required)
-):
-    response.delete_cookie("access_token")
-    response.delete_cookie("refresh_token")
-
-    return LogoutOutputSchema(
-        msg="Logout successfully"
-    )
+        return {"access_token": access_token, "refresh_token": refresh_token}
--- src/auth/schemas/auth_schema.py
+++ src/auth/schemas/auth_schema.py
@@ -1,5 +1,6 @@
 import string
 from datetime import datetime, timezone
+
 from pydantic import BaseModel, EmailStr, Field, field_validator, model_validator
 
 
@@ -23,13 +24,13 @@ class RegisterInputSchema(BaseModel):
                 "La contraseña debe contener al menos un carácter especial"
             )
         return value
-    
+
     @model_validator(mode="after")
     def validate_confirm_password(self):
         if self.password != self.confirm_password:
             raise ValueError("Password not match")
         return self
-    
+
 
 class RegisterOutputSchema(BaseModel):
     msg: str = Field(..., examples=["Register successfully"])
@@ -55,7 +56,7 @@ class LoginInputSchema(BaseModel):
 class LoginOutputSchema(BaseModel):
     access_token: str
     refresh_token: str
-    
+
 
 class LogoutOutputSchema(BaseModel):
     msg: str = Field(..., examples=["Logout successfully"])
@@ -68,17 +69,12 @@ class UserCreateSchema(BaseModel):
     bio: str
     email: EmailStr
     password: str
-    date_created: datetime = Field(
-        default_factory=lambda: datetime.now(timezone.utc)
-    )
-    date_updated: datetime = Field(
-        default_factory=lambda: datetime.now(timezone.utc)
-    )
+    date_created: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    date_updated: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
-    @field_validator("first_name", "last_name", "username", "bio", mode='before')
+    @field_validator("first_name", "last_name", "username", "bio", mode="before")
     @classmethod
     def convert_to_uppercase(cls, value):
         if isinstance(value, str):
             return value.upper()
         return value
-    
