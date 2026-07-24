8a7c9b74c56ff7eb7aca9fdbeef33c20542b60c3
Folco Carril <folco.carril@gmail.com>
2026-07-24 17:21:46 -0300

    feat: The router instance was moved to another file.

---
+++ src/core/routers.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+from auth.controllers.auth_controller import router as auth_router
+from collaborators.controllers.collaborators_controller import (
+    router as collaborators_router,
+)
+from project_invitations.controllers.project_invitation_controller import (
+    router as project_invitation_router,
+)
+from projects.controllers.project_controller import router as project_router
+from projects_tags.controllers.project_tag_controller import (
+    router as project_tag_router,
+)
+from requests.controllers.requests_controller import router as requests_router
+
+
+def register_routers(app: FastAPI) -> None:
+    app.include_router(auth_router)
+    app.include_router(project_router)
+    app.include_router(project_tag_router)
+    app.include_router(collaborators_router)
+    app.include_router(project_invitation_router)
+    app.include_router(requests_router)
