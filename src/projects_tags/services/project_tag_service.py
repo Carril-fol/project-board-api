2dabc2211c8a82b6fd5d74ba9b6f762bc99abce6
Folco Carril <folco.carril@gmail.com>
2026-07-24 20:30:15 -0300

    Update project_tag_service.py
    feat: Added exceptions created

--- src/projects_tags/services/project_tag_service.py
+++ src/projects_tags/services/project_tag_service.py
@@ -1,6 +1,6 @@
 from projects.repositories.project_repository import ProjectRepository
 
-from ..exceptions import ProjectTagNotFound
+from ..exceptions import ProjectNotFound, ProjectTagNotFound
 from ..models.project_tag_model import ProjectTag
 from ..repositories.project_tag_repository import ProjectTagRepository
 from ..schemas.project_tag_schema import CreateProjectTag, RegisterProjectTagInputSchema
@@ -17,23 +17,29 @@ class ProjectTagService:
             raise ProjectTagNotFound("Project tag not found")
         return project_tag
 
+    def _ensure_is_owner(self, project, user_id: int) -> None:
+        if int(project.owner_id) != int(user_id):
+            raise PermissionError("Only the project owner can perform this action")
+
     def create_project_tag(
         self, id_project: int, data: RegisterProjectTagInputSchema, user_id: int
-    ):
+    ) -> None:
         project = self.project_repo.get_project_by_id(id_project)
         if not project:
-            raise ProjectTagNotFound("Project not found")
+            raise ProjectNotFound("Project not found")
 
-        if project.owner_id != user_id:
-            raise ProjectTagNotFound("Project not found")
+        self._ensure_is_owner(project, user_id)
 
         project_tag_data = CreateProjectTag(tag=data.tag, id_project=id_project)
         project_to_create = ProjectTag(**project_tag_data.model_dump())
-        return self.repo.create(project_to_create)
+        self.repo.create(project_to_create)
 
-    def delete_tag_from_project(self, id_tag: int, user_id: int):
+    def delete_tag_from_project(self, id_tag: int, user_id: int) -> None:
         project_tag = self._get_project_tag_or_raise(id_tag)
-        if project_tag.id_project != user_id:
-            raise ProjectTagNotFound("Project tag not found")
 
-        return self.repo.delete(project_tag)
+        project = self.project_repo.get_project_by_id(project_tag.id_project)
+        if not project:
+            raise ProjectNotFound("Project not found")
+
+        self._ensure_is_owner(project, user_id)
+        self.repo.delete(project_tag)
