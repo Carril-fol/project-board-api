ef19346aa32e71844e8ba22cbf10e14824a9149c
Folco Carril <folco.carril@gmail.com>
2026-07-24 17:35:33 -0300

    chore(Auth): Modified auth service with identation changes

--- src/auth/services/auth_services.py
+++ src/auth/services/auth_services.py
@@ -1,41 +1,36 @@
 from core.security.password_manager import PasswordManager
-
-from users.repositories.user_repository import UserRepository
 from users.models.user import User
+from users.repositories.user_repository import UserRepository
 
 from ..exceptions.auth_exception import InvalidCredentialsException
-from ..schemas.auth_schema import RegisterInputSchema, LoginInputSchema, UserCreateSchema
+from ..schemas.auth_schema import (
+    LoginInputSchema,
+    RegisterInputSchema,
+    UserCreateSchema,
+)
 
 
 class AuthService:
-
-    def __init__(
-            self, 
-            user_repo: UserRepository, 
-            password_manager: PasswordManager
-        ):
+    def __init__(self, user_repo: UserRepository, password_manager: PasswordManager):
         self.user_repo = user_repo
         self.password_manager = password_manager
 
     def _get_user_by_email_or_raise(self, email: str):
-        user = self.user_repo.get_user_by_email(email)        
+        user = self.user_repo.get_user_by_email(email)
         return user
 
     def register_user(self, data: RegisterInputSchema):
         if self._get_user_by_email_or_raise(data.email):
             raise Exception("Email already assigned to user")
-
         hashed_password = self.password_manager.hash(data.password)
-
         user_create = UserCreateSchema(
             first_name=data.first_name,
             last_name=data.last_name,
             username=data.username,
             bio=data.bio,
             email=data.email,
-            password=hashed_password
+            password=hashed_password,
         )
-
         user_entity = User(**user_create.model_dump())
         return self.user_repo.create(user_entity)
 
@@ -43,8 +38,6 @@ class AuthService:
         user = self._get_user_by_email_or_raise(data.email)
         if not user:
             raise InvalidCredentialsException("Invalid credentials")
-        
         if not self.password_manager.verify(data.password, user.password):
             raise InvalidCredentialsException("Invalid credentials")
-
         return user.id
