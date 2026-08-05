5721ce877156b1878920f8256d5486169bfdb9b9
Folco Carril <folco.carril@gmail.com>
2026-08-05 17:43:22 -0300

    feat: added function to handler exceptions from comments

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from core.exception_handlers import (
     register_exception_handlers,
     register_invitation_exception_handlers,
     register_project_exception_handlers,
-    register_request_exception_handlers,
+    register_comments_exception_handlers,
+    
 )
 from core.lifespan import lifespan
 from core.routers import register_routers
@@ -18,7 +19,7 @@ def create_app() -> FastAPI:
 
     register_project_exception_handlers(app)
     register_invitation_exception_handlers(app)
-    register_request_exception_handlers(app)
+    register_comments_exception_handlers(app)
     register_exception_handlers(app)
 
     register_routers(app)
@@ -28,4 +29,8 @@ def create_app() -> FastAPI:
 app = create_app()
 
 if __name__ == "__main__":
-    run(app)
+    run(
+        app,
+        #host="0.0.0.0",
+        port=8000,
+    )
