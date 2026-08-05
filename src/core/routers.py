6c42b73435bacbc48d2fcec2142254058f0d09a2
Folco Carril <folco.carril@gmail.com>
2026-08-05 04:24:00 -0300

    feat: added comment and task router

--- src/core/routers.py
+++ src/core/routers.py
@@ -1,18 +1,13 @@
 from fastapi import FastAPI
 
 from auth.controllers.auth_controller import router as auth_router
-from collaborators.controllers.collaborators_controller import (
-    router as collaborators_router,
-)
-from project_invitations.controllers.project_invitation_controller import (
-    router as project_invitation_router,
-)
+from collaborators.controllers.collaborators_controller import router as collaborators_router
+from project_invitations.controllers.project_invitation_controller import router as project_invitation_router
 from projects.controllers.project_controller import router as project_router
-from projects_tags.controllers.project_tag_controller import (
-    router as project_tag_router,
-)
+from projects_tags.controllers.project_tag_controller import router as project_tag_router
 from requests.controllers.requests_controller import router as requests_router
-
+from tasks.controllers.task_controller import router as tasks_router
+from comments.controllers.comment_controller import router as comments_router
 
 def register_routers(app: FastAPI) -> None:
     app.include_router(auth_router)
@@ -21,3 +16,5 @@ def register_routers(app: FastAPI) -> None:
     app.include_router(collaborators_router)
     app.include_router(project_invitation_router)
     app.include_router(requests_router)
+    app.include_router(tasks_router)
+    app.include_router(comments_router)
