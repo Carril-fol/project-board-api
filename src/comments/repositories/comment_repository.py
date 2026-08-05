30fcbc246b55cb6a1e63508ba1d00307b9e437ad
Folco Carril <folco.carril@gmail.com>
2026-08-05 17:31:51 -0300

    feat(comment repository): added functions in repo

--- src/comments/repositories/comment_repository.py
+++ src/comments/repositories/comment_repository.py
@@ -1,15 +1,29 @@
-from sqlalchemy import select
+from sqlalchemy import select, func, exists
+from sqlalchemy.orm import joinedload
 
 from shared.database.base_repository import BaseRepository
 from ..models.comment_model import Comment
-
+from tasks.models.task_model import Task
+from collaborators.models.collaborators_model import Collaborators
 
 class CommentRepository(BaseRepository):
-    
+
     def __init__(self, session):
         super().__init__(Comment, session)
 
-    def get_comments_by_task_id(self, task_id: int):
-        stmt = select(Comment).where(Comment.task_id == task_id)
+    def get_comments_by_task_id(self, task_id: int, limit: int = 20, offset: int = 0):
+        stmt = select(Comment).where(Comment.task_id == task_id).limit(limit).offset(offset)
         result = self.db.execute(stmt).scalars().all()
         return result
+
+    def get_comments_count_by_task_id(self, task_id: int):
+        stmt = select(func.count(Comment.id)).where(Comment.task_id == task_id)
+        return self.db.execute(stmt).scalar()
+
+    def verify_user_access_to_task(self, user_id: int, task_id: int) -> bool:
+        stmt = select(exists().where(
+            Collaborators.id_user == user_id,
+            Collaborators.id_project == Task.project_id,
+            Task.id == task_id
+        ))
+        return self.db.execute(stmt).scalar()
