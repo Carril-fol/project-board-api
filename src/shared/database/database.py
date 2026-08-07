315fe5e51a5c5687fd6cae6cc4791ba4767133d6
Folco Carril <folco.carril@gmail.com>
2026-08-06 22:42:55 -0300

    feat(database): add to_dict method to Base class for serializing model instances

--- src/shared/database/database.py
+++ src/shared/database/database.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from collections.abc import Generator
+from enum import Enum
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
@@ -21,7 +23,22 @@ SessionLocal = sessionmaker(
 )
 
 class Base(DeclarativeBase):
-    pass
+    
+    def to_dict(self):
+        data = {}
+
+        for column in self.__table__.columns:
+            value = getattr(self, column.name)
+
+            if isinstance(value, datetime):
+                value = value.isoformat()
+
+            elif isinstance(value, Enum):
+                value = value.value
+
+            data[column.name] = value
+            
+        return data
 
 
 
